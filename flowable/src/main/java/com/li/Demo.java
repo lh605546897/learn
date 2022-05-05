08e0cef73ca74beb938779b15b4d95bbc4c94ca7
lihua <lh12345>
2022-05-05 11:14:07 +0800

    commit flowable

---
+++ flowable/src/main/java/com/li/Demo.java
@@ -0,0 +1,25 @@
+package com.li;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
